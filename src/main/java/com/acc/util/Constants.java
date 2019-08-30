65610cb421bdc11a3980e982e04df386d4ab3842
xugs <xugshang@163.com>
2019-08-30 14:16:22 +0800

    修改图片路径

--- src/main/java/com/acc/util/Constants.java
+++ src/main/java/com/acc/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public final static String honorImgPath  = "res/images/honorImg/";
     public final static String memberImgPath  = "res/images/memberImg/";
     public final static String proImgPath  = "res/images/proImg/";
-    public final static String proDetailImgPath  = "res/images/proDetailImgPath/";
+    public final static String proDetailImgPath  = "res/images/proDetailImg/";
     public final static String recruitImgPath  = "res/images/recruitImg/";
     public final static String proVideoPath  = "res/video/proVideo/";
 	public final static String LOGINUSER = "SESSIONLOGINUSER_WEB";
